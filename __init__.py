2fcb64625d378f2c929b73f5be14e6d73182aeb3
wassila1300 <wferhani@myges.fr>
2024-09-12 16:20:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ def hello_world():
     return render_template('hello.html') #comm2
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return render_template("contact.html")
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
