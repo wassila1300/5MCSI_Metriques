e5f47357ed98dc05c5a78273dd1a5fad3c589132
wassila1300 <wferhani@myges.fr>
2024-09-12 17:04:55 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
+
 @app.route("/contact/")
 def MaPremiereAPI():
     return render_template("contact.html")
+
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -24,17 +26,15 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique1.html")
+
 @app.route("/histogramme/")
 def mongraphique():
     return render_template("graphique.html")
-@app.route('/extract-minutes/<date_string>')
-def extract_minutes(date_string):
-        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-        minutes = date_object.minute
-        return jsonify({'minutes': minutes})
+
   
 if __name__ == "__main__":
   app.run(debug=True)
