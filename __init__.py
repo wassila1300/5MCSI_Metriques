f465a722877ce2a4b59c196a395c56ea034d69ff
wassila1300 <wferhani@myges.fr>
2024-09-12 17:00:55 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,7 +26,15 @@ def meteo():
     return jsonify(results=results)
 @app.route("/rapport/")
 def mongraphique():
+    return render_template("graphique1.html")
+@app.route("/histogramme/")
+def mongraphique():
     return render_template("graphique.html")
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
   
 if __name__ == "__main__":
   app.run(debug=True)
